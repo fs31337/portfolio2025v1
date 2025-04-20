662bb7f2505add64d9b5a2dce22d967242228836
Franco <francodamiansalina@gmail.com>
2025-04-19 23:34:29 -0300

    init

---
+++ script.js
@@ -0,0 +1,27 @@
+gsap.registerPlugin(ScrollTrigger);
+
+/**Horizontal Scroll */
+
+const races = document.querySelector(".projects");
+
+function getScrollAmount() {
+  let racesWidth = races.scrollWidth;
+  return -(racesWidth - window.innerWidth);
+}
+
+const tween = gsap.to(races, {
+  x: getScrollAmount,
+  duration: 3,
+  ease: "none",
+});
+
+ScrollTrigger.create({
+  trigger: ".projectsWrapper",
+  start: "top top",
+  end: () => `+=${getScrollAmount() * -1}`,
+  pin: true,
+  animation: tween,
+  scrub: 1,
+  invalidateOnRefresh: true,
+  markers: false,
+});
